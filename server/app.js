0a41770485b7b494c8943c5736128b6df16959b8
Chenna <bkesavan488@gmail.com>
2021-12-12 16:50:42 +0530

    uncommented

--- server/app.js
+++ server/app.js
@@ -1,21 +1,21 @@
 import express from 'express';
-// import path from 'path';
-// import cookieParser from 'cookie-parser';
-// import logger from 'morgan';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
 
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
 
 var app = express();
 
-// app.use(logger('dev'));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, '../public')));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.listen(3002);
 
-// export default app;
+export default app;
